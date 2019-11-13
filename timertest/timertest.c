d0fc6261cacff9f8bc3f0466463c9baf987bf544
yugecin <yugecin@users.noreply.github.com>
2019-11-14 00:11:59 +0100

    fix timertest linux impl

--- timertest/timertest.c
+++ timertest/timertest.c
@@ -6,12 +6,18 @@ found in amxtime.h, which is Copyright (c) ITB CompuPhase, 2001-2005
 
 #define ISWIN defined __WIN32__ || defined _WIN32 || defined WIN32
 
-#include <stdio.h>
-#include <time.h>
 #if ISWIN
-  #include <windows.h>
-  #include <mmsystem.h>
+	#include <windows.h>
+	#include <mmsystem.h>
+	#include <stdio.h>
+#else
+	#include <sys/time.h>
+	#define __stdcall
+	#define printf logprintf
+	#define NULL 0
 #endif
+
+typedef void (*logprintf_t)(char* format, ...);
 
 static
 void inittimer()
@@ -29,12 +35,10 @@ unsigned long gettimestamp()
 #if ISWIN
 	value = (unsigned long) timeGetTime();
 #else
-	value = (unsigned long) clock();
-#if CLOCKS_PER_SEC != 1000
-	/* convert to milliseconds */
-	value = 1000L * (value + CLOCKS_PER_SEC / 2);
-	value /= CLOCKS_PER_SEC;
-#endif
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	value = (unsigned long) (tv.tv_sec * 1000L);
+	value += (unsigned long) (tv.tv_usec / 1000L);
 #endif
 	return value;
 }
@@ -44,17 +48,23 @@ unsigned int __stdcall Supports()
 	return 0x20000 | 0x200;
 }
 
+logprintf_t logprintf;
+
 unsigned long startvalue;
+
 unsigned long lastsecond;
 unsigned long lastminute;
 int seconds;
 int minutes;
 
-int __stdcall Load(void *data)
+int __stdcall Load(void **data)
 {
+	logprintf = (logprintf_t) data[0];
 	inittimer();
-	startvalue = lastsecond = lastminute = gettimestamp();
+	startvalue = gettimestamp();
+	lastsecond = lastminute = startvalue;
 	seconds = minutes = 0;
+	printf("initial value %ld\n", startvalue);
 	return 1;
 }
 
@@ -65,22 +75,23 @@ void __stdcall Unload()
 void __stdcall ProcessTick()
 {
 	unsigned long t = gettimestamp();
-	while (t - lastsecond > 1000) {
+	if (t - lastsecond > 1000) {
+		printf("value %ld\n", );
 		printf("second %d\n", seconds++);
 		lastsecond += 1000;
 	}
-	while (t - lastminute > 60000) {
+	if (t - lastminute > 60000) {
 		printf("minute %d\n", minutes++);
 		lastminute += 60000;
 	}
 }
 
-int __stdcall AmxLoad(struct AMX *amx)
+int __stdcall AmxLoad(void *amx)
 {
 	return 0;
 }
 
-int __stdcall AmxUnload(struct AMX *amx)
+int __stdcall AmxUnload(void *amx)
 {
 	return 0;
 }
